b95f61c9b843995ed748da8f0b559344cdea7b03
Charles Martin Reed <charlesmartinreed@icloud.com>
2018-12-27 18:22:00 -0600

    Implement method to reflect distance between rider and driver on map view, with annotation pins.

--- Cabbie/RiderViewController.swift
+++ Cabbie/RiderViewController.swift
@@ -36,6 +36,7 @@ class RiderViewController: UIViewController, CLLocationManagerDelegate, DriverVi
 
         initializeLocationManager()
         checkForPendingCabbieRide()
+        print("\(driverLocation.latitude)", "\(driverLocation.longitude)")
         
     }
     
@@ -58,6 +59,7 @@ class RiderViewController: UIViewController, CLLocationManagerDelegate, DriverVi
                 self.cabHasBeenCalled = true
                 self.callCabbieButton.setTitle("Cancel Cabbie", for: .normal)
                 Database.database().reference().child("RideRequests").removeAllObservers()
+                //self.checkForDriverLocationChange()
                 
                 //reflect how far away the driver is on the rider's UI
                 if let rideRequestDictionary = snapshot.value as? [String:Any] {
@@ -67,6 +69,9 @@ class RiderViewController: UIViewController, CLLocationManagerDelegate, DriverVi
                             self.driverLocation = CLLocationCoordinate2D(latitude: driverLat, longitude: driverLong)
                             self.driverIsOnTheWay = true
                             self.displayDriverAndRiderOnMap()
+                            
+                            //reflect changes to the driver's location on the mapView, in realtime
+                            self.checkForDriverLocationChange()
                         }
                     }
                 }
@@ -74,12 +79,52 @@ class RiderViewController: UIViewController, CLLocationManagerDelegate, DriverVi
         }
     }
     
+    private func checkForDriverLocationChange() {
+        if let email = Auth.auth().currentUser?.email {
+            Database.database().reference().child("RideRequests").queryOrdered(byChild: "email").queryEqual(toValue: email).observe(.childChanged) { (snapshot) in
+                if let rideRequestDictionary = snapshot.value as? [String:Any] {
+                    if let driverLat = rideRequestDictionary["driverLat"] as? Double {
+                        if let driverLong = rideRequestDictionary["driverLong"] as? Double {
+                            //we know the driver is coming, update the driver location
+                            self.driverLocation = CLLocationCoordinate2D(latitude: driverLat, longitude: driverLong)
+                            self.driverIsOnTheWay = true
+                            self.displayDriverAndRiderOnMap()
+                            //we don't end the observance here because we always want to know if this location is changin
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+    
     private func displayDriverAndRiderOnMap() {
         //show both driver and rider on the map to illustrate distance between them
         let driverCLLocation = CLLocation(latitude: driverLocation.latitude, longitude: driverLocation.longitude)
         let riderCLLocation = CLLocation(latitude: userLocation.latitude, longitude: userLocation.longitude)
+        
         let distance = distanceBetweenDriverAndRider(driver: driverCLLocation, rider: riderCLLocation)
-    
+        
+        //setup the map
+        mapView.removeAnnotations(mapView.annotations)
+        let latDelta = abs(driverLocation.latitude - userLocation.latitude) * 2 + 0.005
+        let lonDelta = abs(driverLocation.longitude - userLocation.longitude) * 2 + 0.005
+        let span = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: lonDelta)
+        let region = MKCoordinateRegion(center: userLocation, span: span)
+        
+        mapView.setRegion(region, animated: true)
+        
+        //set up map annotations
+        let riderAnno = MKPointAnnotation()
+        riderAnno.coordinate = userLocation
+        riderAnno.title = "Your Location"
+        
+        let driverAnno = MKPointAnnotation()
+        driverAnno.coordinate = driverLocation
+        driverAnno.title = "Your Driver"
+        
+        mapView.addAnnotation(riderAnno)
+        mapView.addAnnotation(driverAnno)
     }
     
     //MARK:- DriverViewController protocol method
@@ -101,21 +146,27 @@ class RiderViewController: UIViewController, CLLocationManagerDelegate, DriverVi
             
             //find the center of where the user is located and update the map accordingly
             let center = CLLocationCoordinate2D(latitude: coordinates.latitude, longitude: coordinates.longitude)
-            let span = MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02)
-            let region = MKCoordinateRegion(center: center, span: span)
-            
             //set the user location
             userLocation = center
             
-            mapView.setRegion(region, animated: true)
-            
-            //create a mapAnnotation
-            mapView.removeAnnotations(mapView.annotations)
-            
-            let annotation = MKPointAnnotation()
-            annotation.coordinate = center
-            annotation.title = "Current Location"
-            mapView.addAnnotation(annotation)
+            //check whether or not cabbie is on the way
+            if cabHasBeenCalled {
+                displayDriverAndRiderOnMap()
+                //if driver location changes, update distance values on map accordingly
+            } else {
+                //display only the rider
+                let span = MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02)
+                let region = MKCoordinateRegion(center: center, span: span)
+                mapView.setRegion(region, animated: true)
+                
+                //create a mapAnnotation
+                mapView.removeAnnotations(mapView.annotations)
+                
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = center
+                annotation.title = "Current Location"
+                mapView.addAnnotation(annotation)
+            }
         }
     }
     
